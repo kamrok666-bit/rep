ca4ff1f56ead2176f2850bdbe7c13cc5d18b7ae0
kamrok666-bit <kamrok666@gmail.com>
2026-03-05 16:39:14 +0500

    Add files via upload

---
+++ vevid/js/scripts.js
@@ -0,0 +1,115 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 4,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+
+});
+const swiper2 = new Swiper('.swiper-2', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 18,
+    spaceBetween: 5,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: true,
+    },
+    speed: 5000,
+
+});
+const swiper3 = new Swiper('.mySwiper3', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+
+
+
+
+
+
+window.onload = function () {
+    let block = document.querySelectorAll('.section-5__item')
+    let btn = document.querySelector('.section-5__button')
+    for (let i = 8; i < block.length; i++) {
+        block[i].classList.toggle('act');
+
+    }
+    btn.addEventListener('click', function () {
+        for (let i = 8; i < block.length; i++) {
+            block[i].classList.toggle('act');
+
+        }
+        /* block.classList.toggle('act'); */
+    })
+}
+
+
+
+/* --------------Form---------------- */
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+    
+    async function formSend(e) {
+        e.preventDefault();
+        
+        let error = formValidate(form);
+    }
+
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++
+                }
+            }else if(input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input);
+                error++
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++
+                }
+            }
+            
+        }
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    //Функция теста email
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+    return
+})
---
+++ стройкод/js/scripts.js
@@ -0,0 +1,58 @@
+//-------Меню Бургер---------//
+const iconMenu = document.querySelector('.header-menu-icon');
+const menuBody = document.querySelector('.header-menu');
+const autorization = document.querySelector('.autorization');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+        autorization.classList.toggle('_active');
+    });
+}
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.section-4-button__next',
+    prevEl: '.section-4-button__prev',
+  },
+});
+
+
+
+
+
+/* ------------Аккордион---------------- */
+
+document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
+    item.addEventListener('click', () => {
+        const parent = item.parentNode;
+        let a = parent.querySelector('.accordion-item__content')
+
+
+        if (parent.classList.contains('active')) {
+            parent.classList.remove('active');
+            a.style.height = '0px';
+        } else {
+            document.querySelectorAll('.accordion-item').forEach((child) => {
+                child.classList.remove('active')
+                child.querySelector('.accordion-item__content').style.height = '0px';
+            }
+            )
+            parent.classList.add('active');
+            a.style.height = a.scrollHeight + 'px';
+
+        }
+    })
+)
+
